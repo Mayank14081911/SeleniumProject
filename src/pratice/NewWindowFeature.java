fad9009dab0dddac1405844e75b4f6856db2598b
Mayank <stupid11mak@gmail.com>
2019-11-25 00:15:48 +0530

    first project commit

---
+++ src/pratice/NewWindowFeature.java
@@ -0,0 +1,21 @@
+package pratice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver.WindowType;
+
+public class NewWindowFeature {
+
+	public static void main(String[] args) {
+	
+System.setProperty("webdriver.chrome.driver","C:\\Users\\mak\\workspace\\pratice\\chromedriver.exe");
+
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("https://google.com");
+	
+	driver.switchTo().window(null);
+	
+	}
+
+}
